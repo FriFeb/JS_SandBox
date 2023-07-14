6a3e7dd70d0383529b4dd51c68b7c4effc2fe8b7
Fedir Denysenko <feddenisenko1@gmail.com>
2023-07-14 11:21:36 +0300

    Completed Section 5

---
+++ 04-logic-control-flow/04-calculator-challenge/script.js
@@ -0,0 +1,30 @@
+function calculator(num1, num2, operator) {
+
+    switch (operator) {
+
+        case '+':
+            console.log(num1 + num2);
+            break;
+
+        case '-':
+            console.log(num1 - num2);
+            break;
+
+        case '*':
+            console.log(num1 * num2);
+            break;
+        
+        case '/':
+            console.log(num1 / num2);
+            break;
+
+        default:
+            console.log("An error occured! Please enter the operator correctly");
+    }
+}
+
+calculator(5, 2, '+') // returns 7
+calculator(5, 2, '-') // returns 3
+calculator(5, 2, '*') // returns 10
+calculator(5, 2, '/') // returns 2.5
+calculator(5, 2, '&') // returns an error message

c285e542cd9b88c300eadaa5b37c09a31e83ffca
Fedir Denysenko <feddenisenko1@gmail.com>
2023-07-15 18:54:19 +0300

    Completed Removing product(s)

--- 08-shopping-list-project/shopping-list-start/script.js
+++ 08-shopping-list-project/shopping-list-start/script.js
@@ -1,6 +1,28 @@
 const form = document.querySelector('#item-form');
-const input = document.querySelector('#item-input');
+const inputForm = document.querySelector('#item-input');
 const list = document.querySelector('#item-list');
+const clearButton = document.querySelector('#clear');
+
+//      //      //      Adding product      \\      \\      \\
+function addItem(e) {
+    e.preventDefault();
+
+    if (inputForm.value.trim() === '') {
+        alert('Please enter a product name!');
+        return;
+    }
+
+    const listElement = document.createElement('li');
+    listElement.appendChild(document.createTextNode(inputForm.value));
+
+    btn = createButton('remove-item btn-link text-red');
+
+    listElement.appendChild(btn);
+    list.appendChild(listElement);
+
+    inputForm.value = '';
+
+}
 
 function createButton(className) {
     const button = document.createElement('button');
@@ -16,23 +38,30 @@ function createIcon(className) {
     return icon;
 }
 
-function submitForm(e) {
-    e.preventDefault();
+//      //      //      Deleting product(s)      \\      \\      \\
+function deleteItem(e) {
+    const icons = list.querySelectorAll('i');
 
-    if (input.value.trim() === '') {
-        alert('Please enter a product name!');
-        return;
-    }
+    icons.forEach(icon => {
+        if (e.target === icon) {
+            e.target.parentElement.parentElement.remove();
+        };
+    });
 
-    const listElement = document.createElement('li');
-    listElement.appendChild(document.createTextNode(input.value));
-
-    btn = createButton('remove-item btn-link text-red');
-
-    listElement.appendChild(btn);
-    list.appendChild(listElement);
-
-    input.value = '';
 }
 
-form.addEventListener('submit', submitForm);
+function clearItems() {
+    //list.innerHTML = '';
+    if (confirm('Are you sure you want to clear all items from the list?')) {
+        while (list.firstChild) {
+            list.firstChild.remove();
+        }
+    }
+
+}
+
+
+//      //      //      Event listeners      \\      \\      \\
+form.addEventListener('submit', addItem);
+list.addEventListener('click', deleteItem);
+clearButton.addEventListener('click', clearItems);
